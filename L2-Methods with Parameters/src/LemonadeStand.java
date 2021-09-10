08c0aa4600a8bc26c7b7a4649638c5215b16443c
thomaami000 <thomaami000@warren.k12.in.us>
2021-09-10 13:07:32 -0400

    circle calc. & lemonade stand!!

--- L2-Methods with Parameters/src/LemonadeStand.java
+++ L2-Methods with Parameters/src/LemonadeStand.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class LemonadeStand {
 
     /*
@@ -14,9 +17,34 @@ public class LemonadeStand {
         Your program should include at least ONE method with two parameters.
 
      */
+            public static double _16Price = 0.5;
+            public static double _20Price = 0.99;
+            public static double tax = 0.075;
+
 
     public static void main(String[] args) {
+        int numOf16 = Integer.parseInt(JOptionPane.showInputDialog("How many 16oz cups of lemonade do you want to buy?"));
+        int numOf20 = Integer.parseInt(JOptionPane.showInputDialog("How many 20oz cups of lemonade do you want to buy?"));
 
+        calcCost(numOf16, numOf20);
     }
+
+    public static void calcCost(int numOf16, int numOf20){
+        DecimalFormat round = new DecimalFormat("$#,###.00");
+        double originalTotal = (_16Price * numOf16) + (_20Price * numOf20);
+        double taxTotal = (_16Price + _20Price) * (1 + tax);
+
+        double total = originalTotal - taxTotal;
+
+        String message = "Your order: ";
+        message += "\n Number of 16 oz: " + numOf16;
+        message += "\n Number of 20 oz: " + numOf20;
+        message += "\n\n Your total is: " + round.format(total);
+
+        JOptionPane.showMessageDialog(null, message);
+    }
+
+
+
 
 }
