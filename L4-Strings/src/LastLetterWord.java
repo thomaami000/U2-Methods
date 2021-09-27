4a62c4f659bc13bea0ed68f94dbbe3c2d83e64f6
thomaami000 <thomaami000@warren.k12.in.us>
2021-09-27 12:40:19 -0400

    2.4 practice problems!

--- L4-Strings/src/LastLetterWord.java
+++ L4-Strings/src/LastLetterWord.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class LastLetterWord {
 
     /*
@@ -7,11 +9,26 @@ public class LastLetterWord {
     Use a method to find and return the last three letters and a string parameter.
 
     Ex:  Goodbye, Hello, Morning -> byelloing
-    */
+
+     PLAN:
+     1. get 3 inputs
+     2. isolate last three letters of each word
+     3. combine all three endings
+     4. output
+     */
+
 
     public static void main(String[] args) {
+        String word1 = JOptionPane.showInputDialog("Enter word 1: ");
+        String word2 = JOptionPane.showInputDialog("Enter word 2: ");
+        String word3 = JOptionPane.showInputDialog("Enter word 3: ");
 
+        JOptionPane.showMessageDialog(null, lastThree(word1) + lastThree(word2) + lastThree(word3));
+        System.exit(0);
 
+    }
+    public static String lastThree(String word){
+        return wordLength =(word.length()-3);
 
     }
 
--- L4-Strings/src/SplitPs.java
+++ L4-Strings/src/SplitPs.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class SplitPs {
 
     /*
@@ -5,4 +7,25 @@ public class SplitPs {
 
     "Hippopotamus" -> ppop
      */
-}
+
+    /*
+    1. get the input - p and the next three letters
+    2. isolate p
+    3. isolate the last three letters
+    4. output the letters
+     */
+
+    public static void main(String[] args) {
+
+        String word = JOptionPane.showInputDialog("Enter a word with a p");
+
+        JOptionPane.showMessageDialog(null, splitP(word));
+    }
+    public static String splitP (String word){
+
+        int pIndex = word.indexOf('p');
+
+        return word.substring(pIndex, pIndex + 4);
+        }
+    }
+
